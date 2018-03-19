08f2de8f7526329693cd31d59dca98ec99af0f7f
Ilya Los <los.ilia.ru@gmail.com>
2018-03-19 17:00:02 +0400

    Add master value to configuration file.

--- src/main/scala/SparkSessionWrapper.scala
+++ src/main/scala/SparkSessionWrapper.scala
@@ -1,10 +1,13 @@
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.sql.SparkSession
 
 trait SparkSessionWrapper {
   lazy val spark: SparkSession = {
+    val config = ConfigFactory.parseResources("application.conf")
+
     SparkSession
       .builder()
-      .master("yarn")
+      .master(config.getString("spark-submit.master"))
       .appName("Is retweet tweets classifier")
       .getOrCreate()
   }
