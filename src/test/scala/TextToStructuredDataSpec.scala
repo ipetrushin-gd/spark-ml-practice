711502fd9556bf147efcd5d675e778a639a0381c
Ilya Los <los.ilia.ru@gmail.com>
2018-07-30 12:32:39 +0400

    Update structure in TestSpec file

--- src/test/scala/TextToStructuredDataSpec.scala
+++ src/test/scala/TextToStructuredDataSpec.scala
@@ -6,130 +6,144 @@ import TextToStructuredData.getStructuredDataFromText
 class TextToStructuredDataSpec extends FunSpec with SparkSessionCreator with DatasetComparer {
   import spark.implicits._
 
-  it("Simple general check") {
-    val source = List(
-      "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, en, true)",
-      "List($btc #bitcoin #cryptolife #cryptofamily Good morning, Bitcoin has good support now, he might not instantly jump up,… https://t.co/ok8vMxuE17, en, false)",
-      "List(Earn bitcoin on a daily basis!\n1. Follow @slidecoin\n2. Complete instructions in pinned tweet, en, false)",
-      "List(RT @nodepower_eu: Great YouTube video review about #Node made by Getting Started with Crypto channel.\n\nThank you for such a profound ana…, en, true)",
-      "List(RT @Digitalnaiv: Fundstück: Smart Package: #Blockchain Patent von #Walmart -  via @CIOKurator  https://t.co/pt18IRsjee #Retail #Handel, de, true)",
-      "List(#Kepler is one of bigger #AI #ICO still active (link: https://t.co/aUXxAzChbr) https://t.co/gE5c4A4ID8, en, false)",
-      "List(@evenjangle ATMS is the perfect cryptocurrency., en, false)",
-      "List(.@unit4@unit42_intel はここ6ヵ月で、#仮想通貨 の #マイニング を最終目的とした攻撃活動数の大幅増加を観察。何が攻撃者からの大幅な変化を促進し、業界の著しい動向を生み出しているのでしょうか? 最も多く#マイ… https://t.co/qv2hEfGFA7, ja, false)",
-      "List(RT @eGoldgg: If you need to understand the advantages that Blockchain technology can bring to the eSports betting sector - watch Mario Ovch…, en, true)",
-      "List(ネムコイン(XEM)とは？特徴・仕組み・今後について徹底解説！  仮想通貨メディアBTC Feedビットコインフィード https://t.co/qSPi7jZxAk, ja, false)",
-      "List(9 Stocks to Buy Instead of Bitcoin | InvestorPlace https://t.co/UlJ2GuglpE #markets… https://t.co/KdmrRuJple, en, false)",
-      "List(RT @Kora_Network: #Kora was featured on the Crypto Knights Podcast! Check it out. #financialinclusion #blockchain4good\n\nhttps://t.co/2SBwz…, en, true)"
-    ).toDS
+  describe("A tweets") {
+    describe("when tweets are real") {
+      val source = List(
+        "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, en, true)",
+        "List($btc #bitcoin #cryptolife #cryptofamily Good morning, Bitcoin has good support now, he might not instantly jump up,… https://t.co/ok8vMxuE17, en, false)",
+        "List(Earn bitcoin on a daily basis!\n1. Follow @slidecoin\n2. Complete instructions in pinned tweet, en, false)",
+        "List(RT @nodepower_eu: Great YouTube video review about #Node made by Getting Started with Crypto channel.\n\nThank you for such a profound ana…, en, true)",
+        "List(RT @Digitalnaiv: Fundstück: Smart Package: #Blockchain Patent von #Walmart -  via @CIOKurator  https://t.co/pt18IRsjee #Retail #Handel, de, true)",
+        "List(#Kepler is one of bigger #AI #ICO still active (link: https://t.co/aUXxAzChbr) https://t.co/gE5c4A4ID8, en, false)",
+        "List(@evenjangle ATMS is the perfect cryptocurrency., en, false)",
+        "List(.@unit4@unit42_intel はここ6ヵ月で、#仮想通貨 の #マイニング を最終目的とした攻撃活動数の大幅増加を観察。何が攻撃者からの大幅な変化を促進し、業界の著しい動向を生み出しているのでしょうか? 最も多く#マイ… https://t.co/qv2hEfGFA7, ja, false)",
+        "List(RT @eGoldgg: If you need to understand the advantages that Blockchain technology can bring to the eSports betting sector - watch Mario Ovch…, en, true)",
+        "List(ネムコイン(XEM)とは？特徴・仕組み・今後について徹底解説！  仮想通貨メディアBTC Feedビットコインフィード https://t.co/qSPi7jZxAk, ja, false)",
+        "List(9 Stocks to Buy Instead of Bitcoin | InvestorPlace https://t.co/UlJ2GuglpE #markets… https://t.co/KdmrRuJple, en, false)",
+        "List(RT @Kora_Network: #Kora was featured on the Crypto Knights Podcast! Check it out. #financialinclusion #blockchain4good\n\nhttps://t.co/2SBwz…, en, true)"
+      ).toDS
 
-    val actual = getStructuredDataFromText(source)
+      val actual = getStructuredDataFromText(source)
 
-    val expected = Seq(
-      ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4","en",1),
-      ("$btc #bitcoin #cryptolife #cryptofamily Good morning, Bitcoin has good support now, he might not instantly jump up,… https://t.co/ok8vMxuE17","en",0),
-      ("Earn bitcoin on a daily basis!\n1. Follow @slidecoin\n2. Complete instructions in pinned tweet","en",0),
-      ("RT @nodepower_eu: Great YouTube video review about #Node made by Getting Started with Crypto channel.\n\nThank you for such a profound ana…","en",1),
-      ("RT @Digitalnaiv: Fundstück: Smart Package: #Blockchain Patent von #Walmart -  via @CIOKurator  https://t.co/pt18IRsjee #Retail #Handel","de",1),
-      ("#Kepler is one of bigger #AI #ICO still active (link: https://t.co/aUXxAzChbr) https://t.co/gE5c4A4ID8","en",0),
-      ("@evenjangle ATMS is the perfect cryptocurrency.","en",0),
-      (".@unit4@unit42_intel はここ6ヵ月で、#仮想通貨 の #マイニング を最終目的とした攻撃活動数の大幅増加を観察。何が攻撃者からの大幅な変化を促進し、業界の著しい動向を生み出しているのでしょうか? 最も多く#マイ… https://t.co/qv2hEfGFA7","ja",0),
-      ("RT @eGoldgg: If you need to understand the advantages that Blockchain technology can bring to the eSports betting sector - watch Mario Ovch…","en",1),
-      ("ネムコイン(XEM)とは？特徴・仕組み・今後について徹底解説！  仮想通貨メディアBTC Feedビットコインフィード https://t.co/qSPi7jZxAk","ja",0),
-      ("9 Stocks to Buy Instead of Bitcoin | InvestorPlace https://t.co/UlJ2GuglpE #markets… https://t.co/KdmrRuJple","en",0),
-      ("RT @Kora_Network: #Kora was featured on the Crypto Knights Podcast! Check it out. #financialinclusion #blockchain4good\n\nhttps://t.co/2SBwz…","en",1)
-    ).toDF("text", "language", "label")
+      val expected = Seq(
+        ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4", "en", 1),
+        ("$btc #bitcoin #cryptolife #cryptofamily Good morning, Bitcoin has good support now, he might not instantly jump up,… https://t.co/ok8vMxuE17", "en", 0),
+        ("Earn bitcoin on a daily basis!\n1. Follow @slidecoin\n2. Complete instructions in pinned tweet", "en", 0),
+        ("RT @nodepower_eu: Great YouTube video review about #Node made by Getting Started with Crypto channel.\n\nThank you for such a profound ana…", "en", 1),
+        ("RT @Digitalnaiv: Fundstück: Smart Package: #Blockchain Patent von #Walmart -  via @CIOKurator  https://t.co/pt18IRsjee #Retail #Handel", "de", 1),
+        ("#Kepler is one of bigger #AI #ICO still active (link: https://t.co/aUXxAzChbr) https://t.co/gE5c4A4ID8", "en", 0),
+        ("@evenjangle ATMS is the perfect cryptocurrency.", "en", 0),
+        (".@unit4@unit42_intel はここ6ヵ月で、#仮想通貨 の #マイニング を最終目的とした攻撃活動数の大幅増加を観察。何が攻撃者からの大幅な変化を促進し、業界の著しい動向を生み出しているのでしょうか? 最も多く#マイ… https://t.co/qv2hEfGFA7", "ja", 0),
+        ("RT @eGoldgg: If you need to understand the advantages that Blockchain technology can bring to the eSports betting sector - watch Mario Ovch…", "en", 1),
+        ("ネムコイン(XEM)とは？特徴・仕組み・今後について徹底解説！  仮想通貨メディアBTC Feedビットコインフィード https://t.co/qSPi7jZxAk", "ja", 0),
+        ("9 Stocks to Buy Instead of Bitcoin | InvestorPlace https://t.co/UlJ2GuglpE #markets… https://t.co/KdmrRuJple", "en", 0),
+        ("RT @Kora_Network: #Kora was featured on the Crypto Knights Podcast! Check it out. #financialinclusion #blockchain4good\n\nhttps://t.co/2SBwz…", "en", 1)
+      ).toDF("text", "language", "label")
 
-    assertSmallDataFrameEquality(actual, expected)
-  }
+      it("should be processed normally") {
+        assertSmallDataFrameEquality(actual, expected)
+      }
+    }
 
-  it("IsRetweet check") {
-    val source = List(
-      "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, en, true)",
-      "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, en, false)"
-    ).toDS
+    describe("with isRetweet field") {
+      val source = List(
+        "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, en, true)",
+        "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, en, false)"
+      ).toDS
 
-    val actual = getStructuredDataFromText(source)
+      val actual = getStructuredDataFromText(source)
 
-    val expected = Seq(
-      ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4","en",1),
-      ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4","en",0)
-    ).toDF("text", "language", "label")
+      val expected = Seq(
+        ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4", "en", 1),
+        ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4", "en", 0)
+      ).toDF("text", "language", "label")
 
-    assertSmallDataFrameEquality(actual, expected)
-  }
+      it("true should be replaced with 1, false - with 0") {
+        assertSmallDataFrameEquality(actual, expected)
+      }
+    }
 
-  it("Language check") {
-    val source = List(
-      "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, en, true)",
-      "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, de, true)",
-      "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, jp, true)",
-      "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, fr, true)",
-      "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, ru, false)"
-    ).toDS
+    describe("with language field") {
+      val source = List(
+        "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, en, true)",
+        "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, de, true)",
+        "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, jp, true)",
+        "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, fr, true)",
+        "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, ru, false)"
+      ).toDS
 
-    val actual = getStructuredDataFromText(source)
+      val actual = getStructuredDataFromText(source)
 
-    val expected = Seq(
-      ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4","en",1),
-      ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4","de",1),
-      ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4","jp",1),
-      ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4","fr",1),
-      ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4","ru",0)
-    ).toDF("text", "language", "label")
+      val expected = Seq(
+        ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4", "en", 1),
+        ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4", "de", 1),
+        ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4", "jp", 1),
+        ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4", "fr", 1),
+        ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4", "ru", 0)
+      ).toDF("text", "language", "label")
 
-    assertSmallDataFrameEquality(actual, expected)
-  }
+      it("language should be parsed as separate 2-letters string") {
+        assertSmallDataFrameEquality(actual, expected)
+      }
+    }
 
-  it("New lines check") {
-    val source = List(
-      """List(
-        |RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, en, true)""".stripMargin,
-      """List($btc #bitcoin #cryptolife
-        |
-        |#cryptofamily Good morning, Bitcoin has good support now,
-        |he might not instantly jump up,…
-        |https://t.co/ok8vMxuE17, en, false)""".stripMargin,
-      """List(Earn bitcoin on a daily basis!
-        |1. Follow @slidecoin
-        |2. Complete instructions in pinned tweet, en, false)""".stripMargin
-    ).toDS
+    describe(" with new-line symbols in text") {
+      val source = List(
+        """List(
+          |RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, en, true)""".stripMargin,
+        """List($btc #bitcoin #cryptolife
+          |
+          |#cryptofamily Good morning, Bitcoin has good support now,
+          |he might not instantly jump up,…
+          |https://t.co/ok8vMxuE17, en, false)""".stripMargin,
+        """List(Earn bitcoin on a daily basis!
+          |1. Follow @slidecoin
+          |2. Complete instructions in pinned tweet, en, false)""".stripMargin
+      ).toDS
 
-    val actual = getStructuredDataFromText(source)
+      val actual = getStructuredDataFromText(source)
 
-    val expected = Seq(
-      ("\nRT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4","en",1),
-      ("$btc #bitcoin #cryptolife\n\n#cryptofamily Good morning, Bitcoin has good support now,\nhe might not instantly jump up,…\nhttps://t.co/ok8vMxuE17","en",0),
-      ("Earn bitcoin on a daily basis!\n1. Follow @slidecoin\n2. Complete instructions in pinned tweet","en",0)
-    ).toDF("text", "language", "label")
+      val expected = Seq(
+        ("\nRT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4", "en", 1),
+        ("$btc #bitcoin #cryptolife\n\n#cryptofamily Good morning, Bitcoin has good support now,\nhe might not instantly jump up,…\nhttps://t.co/ok8vMxuE17", "en", 0),
+        ("Earn bitcoin on a daily basis!\n1. Follow @slidecoin\n2. Complete instructions in pinned tweet", "en", 0)
+      ).toDF("text", "language", "label")
 
-    assertSmallDataFrameEquality(actual, expected)
-  }
+      it("should be processed in correct way") {
+        assertSmallDataFrameEquality(actual, expected)
+      }
+    }
 
-  it("Unknown language check") {
-    val source = List(
-      "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, und, true)"
-    ).toDS
+    describe("with 'und' as language marker") {
+      val source = List(
+        "List(RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4, und, true)"
+      ).toDS
 
-    val actual = getStructuredDataFromText(source)
+      val actual = getStructuredDataFromText(source)
 
-    val expected = Seq(
-      ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4","??",1)
-    ).toDF("text", "language", "label")
+      val expected = Seq(
+        ("RT @emy_wng: Thanks Bloomberg for featuring @Ripple and #MoneyTap! https://t.co/WDuJnD1tq4", "??", 1)
+      ).toDF("text", "language", "label")
 
-    assertSmallDataFrameEquality(actual, expected)
-  }
+      it("language should be parsed as '??' (2 question marks)") {
+        assertSmallDataFrameEquality(actual, expected)
+      }
+    }
 
-  it("Empty tweet check") {
-    val source = List(
-      "List(, und, true)"
-    ).toDS
+    describe("without text") {
+      val source = List(
+        "List(, und, true)"
+      ).toDS
 
-    val actual = getStructuredDataFromText(source)
+      val actual = getStructuredDataFromText(source)
 
-    val expected = Seq(
-      ("","??",1)
-    ).toDF("text", "language", "label")
+      val expected = Seq(
+        ("", "??", 1)
+      ).toDF("text", "language", "label")
 
-    assertSmallDataFrameEquality(actual, expected)
+      it("should be parsed in correct way") {
+        assertSmallDataFrameEquality(actual, expected)
+      }
+    }
   }
 }
--- src/test/scala/TweetsNormalizationSpec.scala
+++ src/test/scala/TweetsNormalizationSpec.scala
@@ -7,29 +7,32 @@ import com.github.mrpowers.spark.fast.tests.DatasetComparer
 class TweetsNormalizationSpec extends FunSpec with SparkSessionCreator with DatasetComparer {
   import spark.implicits._
 
-  it("Normalize tweets checking") {
-    val source = spark.createDataset[String](List(
-      "Tweet text, en, true)",
-      "Tweet text, en, false)",
-      "Tweet text, und, true)",
-      "Tweet text, und, false)",
-      "Tweet text, ru, true)",
-      "Tweet text, ru, false)",
-      "false true false und en ru, en, false)"
-    ))
+  describe("Tweets") {
+    describe("with isRetweet field")
+    it("Normalize tweets checking") {
+      val source = spark.createDataset[String](List(
+        "Tweet text, en, true)",
+        "Tweet text, en, false)",
+        "Tweet text, und, true)",
+        "Tweet text, und, false)",
+        "Tweet text, ru, true)",
+        "Tweet text, ru, false)",
+        "false true false und en ru, en, false)"
+      ))
 
-    val actual = source.transform[String](normalizeTweets())
+      val actual = source.transform[String](normalizeTweets())
 
-    val expected = spark.createDataset[String](List(
-      "Tweet text, en, 1",
-      "Tweet text, en, 0",
-      "Tweet text, ??, 1",
-      "Tweet text, ??, 0",
-      "Tweet text, ru, 1",
-      "Tweet text, ru, 0",
-      "false true false und en ru, en, 0"
-    ))
+      val expected = spark.createDataset[String](List(
+        "Tweet text, en, 1",
+        "Tweet text, en, 0",
+        "Tweet text, ??, 1",
+        "Tweet text, ??, 0",
+        "Tweet text, ru, 1",
+        "Tweet text, ru, 0",
+        "false true false und en ru, en, 0"
+      ))
 
-    assertSmallDatasetEquality(actual, expected)
+      assertSmallDatasetEquality(actual, expected)
+    }
   }
 }
